e03cfee04fdc4b3814e340db44f4154448164162
Mengstab Ketemaw <mengstabrealk28@gmail.com>
2022-05-16 09:21:24 -0700

    initial configuration for the data loader.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Mestenagir book loader and fetcher
 ### Guides
 this app is used to load all the book from http://www.ethiopianorthodox.org/amharic/ and store the name, address and category of the book
-in the form of json file (books.json). you can see it in the repository.
-the app will load books with extension of pdf only.
+in the form of json file (books.json). you can see it in the repository. after fetching the file the book will be store on a remote noSql database
+cassandra. the app will load books with extension of pdf only.
--- src/main/java/io/mestenagir/bookloader/BoofFetcherAndLoaderApplication.java
+++ src/main/java/io/mestenagir/bookloader/BoofFetcherAndLoaderApplication.java
@@ -1,17 +1,50 @@
 package io.mestenagir.bookloader;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.mestenagir.bookloader.connection.DataStaxAstraProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.nio.file.Path;
 
-//@SpringBootApplication
+@SpringBootApplication
+@EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class BoofFetcherAndLoaderApplication {
 
 	public static void main(String[] args) throws IOException {
-//		SpringApplication.run(BoofFetcherAndLoaderApplication.class, args);
-		var fetcher = new Fetcher();
-		fetcher.getPdfsAndWriteToFile("http://www.ethiopianorthodox.org/amharic/");
+		SpringApplication.run(BoofFetcherAndLoaderApplication.class, args);
+	}
+	/**
+	 * This is necessary to have the Spring Boot app use the Astra secure bundle
+	 * to connect to the database
+	 */
+
+	@Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
+		Path bundle = astraProperties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
+	}
+
+	@PostConstruct
+	public void fetchingData(){
+//		initFetchData(); // fetch the all the book name.
+//		initAddData(); //add each books in to the database.
+	}
+
+	private void initFetchData() {
+		try {
+			Fetcher fetcher = new Fetcher();
+			fetcher.getPdfsAndWriteToFile("http://www.ethiopianorthodox.org/amharic/");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void initAddData() {
+
 	}
 
 }
